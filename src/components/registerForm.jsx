5e5c80dd98dbf26dfdb51f0a67aab668bb87c188
Augustine Awuori <augustineawuori95@gmail.com>
2021-11-20 08:33:57 +0300

    Update forms.

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import Joi from 'joi-browser';
 import Form from './common/form';
+import { getGenders } from '../services/fakeGenderService';
 
 class RegisterForm extends Form {
 	state = {
-		data: { indexNumber: '', password: '', name: '' },
+		data: { indexNumber: '', password: '', name: '', gender: '' },
+		genders: [],
 		errors: {}
 	};
 
+	componentDidMount() {
+		this.setState({ genders: getGenders() });
+	}
+
 	schema = {
-		indexNumber: Joi.string().required().email().label('Username'),
+		indexNumber: Joi.string().required().label('Index'),
 		password: Joi.string().required().min(5).label('Password'),
-		name: Joi.string().required().label('Name')
+		name: Joi.string().required().label('Name'),
+		gender: Joi.string().required().label('Gender')
 	};
 
 	doSubmit = () => {
@@ -26,6 +33,7 @@ class RegisterForm extends Form {
 				<form onSubmit={this.handleSubmit}>
 					{this.renderInput('indexNumber', 'Index')}
 					{this.renderInput('password', 'Password', 'password')}
+					{this.renderSelect('gender', 'Gender', this.state.genders)}
 					{this.renderInput('name', 'Name')}
 					{this.renderButton('Register')}
 				</form>
---
+++ src/services/fakeGenderService.js
@@ -0,0 +1,3 @@
+export const genders = [ { _id: '13467rs31', name: 'Male' }, { _id: '13467rs32', name: 'Female' } ];
+
+export const getGenders = () => genders.filter((g) => g);
