3a869531db1181174eaa4f434354827721ddff31
Augustine Awuori <augustineawuori95@gmail.com>
2021-11-27 07:27:55 +0300

    Implement navbar

--- src/components/profile.jsx
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class profile extends Component {
-	render() {
-		return <h1>My Profile</h1>;
-	}
-}
--- src/services/fakeGenderService.js
+++
@@ -1,3 +0,0 @@
-export const genders = [ { _id: '13467rs31', name: 'Male' }, { _id: '13467rs32', name: 'Female' } ];
-
-export const getGenders = () => genders.filter((g) => g);
